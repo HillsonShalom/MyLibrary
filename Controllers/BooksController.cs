eb910290541af1132cf67a95d62871adde9b6c7d
Hillson <hillsonshalom@gmail.com>
2024-08-01 13:00:25 +0300

    אחד בצהריים

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -73,15 +73,27 @@ namespace MyLibrary.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken] // זה צריך לשנות מהיסוד
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Name,Height,Width,ShelfId")] Book book)
         {
             if (ModelState.IsValid)
             {
                 book.Shelf = await _context.Shelf.FindAsync(book.ShelfId);
-                _context.Add(book);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (book.Shelf.AvailableWidth >= book.Width && book.Shelf.Height >= book.Height)
+                {
+                    book.Shelf.AvailableWidth -= book.Width;
+                    _context.Add(book);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index), new { id = book.ShelfId });
+                }
+                else
+                {
+                    if (book.Shelf.AvailableWidth < book.Width && book.Shelf.Height < book.Height) ViewData["Message"] = "The shelf is too Small and the book is too high";
+                    else if (book.Shelf.AvailableWidth < book.Width) ViewData["Message"] = "The shelf is too Small";
+                    else if (book.Shelf.Height < book.Height) ViewData["Message"] = "The book is too high";
+                    ViewData["ShelfId"] = book.ShelfId;
+                    return View(book);
+                }
             }
             ViewData["ShelfId"] = book.ShelfId;
             return View(book);
@@ -99,19 +111,37 @@ namespace MyLibrary.Controllers
         {
             model.Books = model.Books.Where(s => s.Name != null && s.Width != 0).ToList();
             Shelf shelf = _context.Shelf.Find(model.ShelfId);
+            double width = 0;
+            foreach (var item in model.Books)
+            {
+                width += item.Width;
+            }
             if (ModelState.IsValid)
             {
-                foreach (var book in model.Books)
+                if (shelf.AvailableWidth >= width && shelf.Height >= model.Height)
                 {
-                    book.SetName = model.SetName;
-                    book.Height = model.Height;
-                    book.Shelf = shelf;
-                    book.ShelfId = model.ShelfId;
-                    _context.Book.Add(book);
+                    shelf.AvailableWidth -= width;
+                    foreach (var book in model.Books)
+                    {
+                        book.SetName = model.SetName;
+                        book.Height = model.Height;
+                        book.Shelf = shelf;
+                        book.ShelfId = model.ShelfId;
+                        _context.Book.Add(book);
+                    }
+                    _context.SaveChanges();
+                    return RedirectToAction("Index", new { id = model.ShelfId });
                 }
-                _context.SaveChanges();
-                return RedirectToAction("Index");
+                else
+                {
+                    if (shelf.AvailableWidth < width && shelf.Height < model.Height) ViewData["Message"] = "The shelf is too Small and the set is to high";
+                    else if (shelf.AvailableWidth < width) ViewData["Message"] = "The shelf is too Small";
+                    else if (shelf.Height < model.Height) ViewData["Message"] = "The book is too high";
+                    ViewData["ShelfId"] = model.ShelfId;
+                    return View(model);
+                }
             }
+            ViewData["ShelfId"] = model.ShelfId;
             return View(model);
         }
 
